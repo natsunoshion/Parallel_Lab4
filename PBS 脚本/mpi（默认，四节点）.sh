865648785f91402c674398c4d5d0ab5ec38edc9a
hsdf1234 <natsunoshion@gmail.com>
2023-06-04 19:52:00 +0800

    MPI programming

---
+++ PBS 脚本/mpi（默认，四节点）.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#PBS -N mpi
+#PBS -l nodes=4
+
+LOG_DIR="/home/ss2113958/MPI"
+
+pssh -h $PBS_NODEFILE mkdir -p $LOG_DIR 1>&2
+scp master:$LOG_DIR/mpi $LOG_DIR
+pscp -h $PBS_NODEFILE $LOG_DIR/mpi $LOG_DIR 1>&2
+mpiexec -np 4 -machinefile $PBS_NODEFILE $LOG_DIR/mpi
---
+++ 普通高斯/普通高斯消去串行.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>  // 使用万能头文件stdc++.h，注意，有的IDE可能不支持
+#include "random.h"  // 使用自制的随机库
+
+using namespace std;
+
+// 普通高斯消去，LU分解
+void LU(float** A, int n) {
+    // cout << "start LU" << endl;
+    for (int k=0; k<n; k++) {
+        for (int j=k+1; j<n; j++) {
+            A[k][j] = A[k][j] / A[k][k];
+        }
+        A[k][k] = 1.0;
+        for (int i=k+1; i<n; i++) {
+            for (int j=k+1; j<n; j++) {
+                A[i][j] = A[i][j] - A[k][j]*A[i][k];
+            }
+            A[i][k] = 0;
+        }
+        // print(A, n);
+        // cout << endl;
+    }
+}
+
+int main() {
+    vector<int> size = {200, 500, 1000, 2000, 3000};
+    for (int i=0; i<5; i++) {
+        // 设置问题规模
+        int n = size[i];
+
+        // 初始化二维数组
+        float** A = new float*[n];
+        for (int i=0; i<n; i++) {
+            A[i] = new float[n];
+        }
+        reset(A, n);
+
+        // 使用C++11的chrono库来计时
+        auto start = chrono::high_resolution_clock::now();
+        LU(A, n);
+        auto end = chrono::high_resolution_clock::now();
+        auto diff = chrono::duration_cast<chrono::duration<double, milli>>(end - start);
+        cout << "Size = " << n << ": " << diff.count() << "ms" << endl;
+
+        // if (n == 1000) {
+        //     print(A, n);
+        //     break;
+        // }
+
+        // 释放二维数组A的空间
+        for (int i = 0; i < n; i++) {
+            delete[] A[i];
+        }
+        delete[] A;
+    }
+    return 0;
+}
